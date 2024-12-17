555fb5f115abad71ea40655ab90c4dce1a3a7918
Exigent07 <aravindhlearner07@gmail.com>
2024-12-17 13:15:31 +0530

    [update] updated keccak.py (Implemented theta)

--- src/sha3/keccak.py
+++ src/sha3/keccak.py
@@ -1,10 +1,16 @@
-from typing import Union
+from typing import Union, List
 import numpy as np
 import gmpy2
 
 class Keccak:
     def __init__(self) -> None:
-        self.__state = [[np.uint64(0) for _ in range(5)] for _ in range(5)]
+        None
+        
+    def __rotate_left(self, x, n):
+        return ((x << n) | (x >> (64 - n))) & np.uint64(0xFFFFFFFFFFFFFFFF)
+
+    def __state_init(self):
+        return [[np.uint64(0) for _ in range(5)] for _ in range(5)]
 
     def __RC_LFSR(self, steps: int) -> int:
         result = np.uint64(0x0000000000000001)
@@ -27,7 +33,7 @@ class Keccak:
         
         return result
     
-    def __pad10star1(self, message: Union[str, bytes], rate: int) -> bytes:
+    def __pad10star1(self, message: Union[str, bytes], rate: int) -> List[bytes]:
         if isinstance(message, str):
             message = message.encode("utf-8")
         
@@ -41,6 +47,28 @@ class Keccak:
         padded_message = int(padded_bits, 2).to_bytes((len(padded_bits) + 7) // 8, 'big')
     
         return padded_message
+    
+    def __absorb_block(self, state, block):
+        for i in range(0, 25):
+            state[i // 5][i % 5] ^= block[i]
+
+        return state
+    
+    def __theta(self, state):
+        C = [np.uint64(0) for _ in range(5)]
+        D = [np.uint64(0) for _ in range(5)]
+        
+        for x in range(5):
+            C[x] = state[x][0] ^ state[x][1] ^ state[x][2] ^ state[x][3] ^ state[x][4]
+
+        for x in range(5):
+            D[x] = C[(x - 1) % 5] ^ self.__rotate_left(C[(x + 1) % 5], 1)
+
+        for x in range(5):
+            for y in range(5):
+                state[x][y] ^= D[x]
+
+        return state
 
 
 if __name__ == "__main__":
